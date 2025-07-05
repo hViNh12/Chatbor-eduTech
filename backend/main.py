8c702cc799c0d51840233ee1762749b953cf71bf
hViNh12 <118531652+hViNh12@users.noreply.github.com>
2025-07-06 02:28:28 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -104,4 +104,4 @@ def ask_question(request: QueryRequest):
 # === Run locally ===
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
